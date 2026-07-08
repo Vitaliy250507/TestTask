dde34272ad430b87afb26611188aa36957821b0b
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-08 15:58:15 +0400

    Added views for comments API

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CommentModel
+from .serializers import CommentCreateSerializer, CommentFetchSerializer
 
-# Create your views here.
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    def get_queryset(self):
+        return CommentModel.objects.filter(parent__isnull=True).select_related("user")
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CommentCreateSerializer
+        return CommentFetchSerializer
