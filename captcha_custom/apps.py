6ac684309b44bb53c57b51f27ebe6e23d333933d
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-08 17:39:54 +0400

    Added captcha_custom

---
+++ captcha_custom/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CaptchaCustomConfig(AppConfig):
+    name = 'captcha_custom'
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "comments",
     "rest_framework",
+    "captcha_custom",
 ]
 
 MIDDLEWARE = [
