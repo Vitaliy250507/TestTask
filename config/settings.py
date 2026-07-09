4a2764828698f556830a2e96f0d378b3c879a8f5
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-10 00:27:11 +0400

    Caches config added

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,15 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+CACHE_TTL = 60 * 15
