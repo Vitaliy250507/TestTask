77a098d8fddda6367dc4092c0eb9cda9c65fca19
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-09 23:56:59 +0400

    JWT configuration added

--- config/settings.py
+++ config/settings.py
@@ -128,8 +128,9 @@ STATIC_URL = "static/"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 MEDIA_URL = "/media/"
