8fe3116176a1337481c2084c18e73ee4ca059cbe
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-10 00:14:04 +0400

    added create token function

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import CommentModel
 from .serializers import CommentCreateSerializer, CommentFetchSerializer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
@@ -24,4 +26,33 @@ class CommentListCreateView(generics.ListCreateAPIView):
         async_to_sync(channel_layer.group_send)(
             "comments_stream",
             {"type": "send_comment", "comment": broadcasting_serializer.data},
+        )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer_class()(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        comment_instance = serializer.save()
+        broadcasting_serializer = CommentFetchSerializer(comment_instance)
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "comments_stream",
+            {"type": "send_comment", "comment": broadcasting_serializer.data},
+        )
+
+        user = comment_instance.user
+        refresh = RefreshToken()
+        refresh["user_id"] = user.id
+        refresh["username"] = user.username
+        refresh["email"] = user.email
+
+        return Response(
+            {
+                "comment": broadcasting_serializer.data,
+                "tokens": {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+            },
+            status=status.HTTP_201_CREATED,
         )
