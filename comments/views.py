84cbb5128c0603d8bfa5327771e1f3ac71bc14bd
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-09 19:27:41 +0400

    Added realtime comments broadcasting via asgi

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import CommentModel
 from .serializers import CommentCreateSerializer, CommentFetchSerializer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
@@ -11,3 +13,15 @@ class CommentListCreateView(generics.ListCreateAPIView):
         if self.request.method == "POST":
             return CommentCreateSerializer
         return CommentFetchSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+
+        broadcasting_serializer = CommentFetchSerializer(instance)
+
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            "comments_stream",
+            {"type": "send_comment", "comment": broadcasting_serializer.data},
+        )
--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+import comments.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_asgi_app = get_asgi_application()
@@ -18,6 +19,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter([])),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(comments.routing.websocket_urlpatterns)
+        ),
     }
 )
