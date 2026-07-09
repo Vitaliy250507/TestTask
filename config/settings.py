e95aa4c76c0aea4cf000e27555da3497266a7886
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-07-10 00:55:37 +0400

    Celery config done

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,8 @@ CACHES = {
 }
 
 CACHE_TTL = 60 * 15
+
+CELERY_BROKER_URL = "redis://redis:6379/2"
+CELERY_RESULT_BACKEND = "redis://redis:6379/2"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
